a64f3effc72a054ddc7a9d07998f1d791b0f28a2
walter-grace <nico.zahniser@gmail.com>
2021-09-06 14:25:41 -0700

    first commit

---
+++ src/Minter.js
@@ -0,0 +1,79 @@
+import { RainbowButton } from "@rainbow-me/rainbow-button";
+import { useEffect, useState } from "react";
+import { connectWallet } from "./utils/interact";
+
+const Minter = (props) => {
+
+  //State variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+ 
+  useEffect(async () => { //TODO: implement
+    
+  }, []);
+
+  const connectWalletPressed = async () => { //TODO: implement
+   
+  };
+
+  const onMintPressed = async () => { //TODO: implement
+    
+  };
+
+  return (
+    <div className="Minter">
+      <RainbowButton
+      chainId={1}
+      connectorOptions={{ bridge: 'https://bridge.walletconnect.org' }}
+      onConnectorInitialized={(connector) => console.log(connector)}
+      render={() =>  <button id="walletButton" onClick={connectWalletPressed}>
+      {walletAddress.length > 0 ? (
+        "Connected: " +
+        String(walletAddress).substring(0, 6) +
+        "..." +
+        String(walletAddress).substring(38)
+      ) : (
+        <span>Connect Wallet</span>
+      )}
+    </button>}
+      />    
+
+      <br></br>
+      <h1 id="title">🌈 NFT Minter</h1>
+      <p>
+        Simply add your asset's link, name, and description, then press "Mint."
+      </p>
+      <form>
+        <h2>🖼 Link to asset: </h2>
+        <input
+          type="text"
+          placeholder="e.g. https://gateway.pinata.cloud/ipfs/<hash>"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>🤔 Name: </h2>
+        <input
+          type="text"
+          placeholder="e.g. My first NFT!"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <h2>✍️ Description: </h2>
+        <input
+          type="text"
+          placeholder="e.g. Even cooler than cryptokitties ;)"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        Mint NFT
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
